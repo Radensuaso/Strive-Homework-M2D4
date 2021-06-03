d0912c220e1a740e305a03d4d2c9334f5a766dcb
Radensuaso <andrels9283@gmail.com>
2021-06-03 19:35:23 +0100

    Adjustments to the buttons
    Managed to put the buttons in the end of the respective containers

--- index.js
+++ index.js
@@ -15,14 +15,29 @@ const generateNumberOfInputs = () => {
   // Attach inputs to the form
   for (let i = 0; i < inputNumber; i++) {
     const inputName = document.createElement("div")
-    inputName.classList.add("input-group", "mb-3", "col-12")
-    inputName.innerHTML = `<input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />`
+    inputName.classList.add("col-12", "col-sm-6", "col-md-4", "col-lg-3")
+    inputName.innerHTML = `<div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      aria-label="Sizing example input"
+      aria-describedby="inputGroup-sizing-default"
+    />
+  </div>`
     inputListForm.appendChild(inputName)
   }
 
   //stop showing numbers form and start showing input for names
-  inputListForm.style.display = "block"
+  inputListForm.style.display = "flex"
   document.querySelector("#generate-number").style.display = "none"
+
+  //create and append submit button
+  const submitButton = document.createElement("div")
+  submitButton.classList.add("col-2")
+  submitButton.innerHTML = `<button type="button" class="btn btn-primary">Submit</button>`
+  inputListForm.appendChild(submitButton)
+  //add event listener to submit button
+  submitButton.addEventListener("click", submitNames)
 }
 
 // function to push names to the array
@@ -39,19 +54,41 @@ const submitNames = () => {
 
   //get The row inside the name-pool div
   const row = document.querySelector("#name-pool div")
-
+  console.log(row)
   //for every name create a node to append to this row
   for (let i = 0; i < namePool.length; i++) {
     const name = namePool[i]
     const nameDiv = document.createElement("div")
-    nameDiv.classList.add("col-6", "col-md-4", "col-lg-3", "border")
+    nameDiv.classList.add(
+      "col-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-3",
+      "py-2",
+      "border"
+    )
     nameDiv.innerText = `${name}`
     row.appendChild(nameDiv)
   }
 
-  //Show pool list
+  //create and append randomize button
+  const randomizeButton = document.createElement("div")
+  randomizeButton.classList.add(
+    "col-12",
+    "col-sm-6",
+    "col-md-4",
+    "col-lg-3",
+    "pt-2"
+  )
+  randomizeButton.innerHTML = `<button type="button" class="btn btn-primary">Randomize</button>`
+  row.appendChild(randomizeButton)
+  //add event listener to randomize button
+  randomizeButton.addEventListener("click", randomize)
+
+  //Show pool list and hide form section
   const namePoolContainer = document.querySelector("#name-pool")
   namePoolContainer.style.display = "block"
+  document.querySelector("#form-section").style.display = "none"
 }
 
 //function to randomize people for each group
@@ -72,8 +109,4 @@ window.onload = () => {
   //generate Button
   const generateButton = document.querySelector("#generate-number button")
   generateButton.addEventListener("click", generateNumberOfInputs)
-
-  //Submit Button
-  const submitButton = document.querySelector("#input-list button")
-  submitButton.addEventListener("click", submitNames)
 }
